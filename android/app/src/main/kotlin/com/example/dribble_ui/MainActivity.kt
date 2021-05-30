6149a66408bd6e3527b3b095a13b761ee66de355
KhineKhineLin <santun2811@gmail.com>
2021-05-30 13:21:57 +0700

    camera shop ui first commit

---
+++ android/app/src/main/kotlin/com/example/dribble_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dribble_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:dribble_ui/camera_shop_app/camera_shop_app_main_page.dart';
+import 'package:flutter/material.dart';
+
+void main()=>
+  runApp(CameraShopApp());
+
+
