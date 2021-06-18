fb07212bd0243b3c9d591b19064cd38a32e18b8e
Khine Lin <santun2811@gmail.com>
2021-06-18 15:04:43 +0630

    create job finder ui first commit

--- lib/job_finder_app/job_finder_main_page.dart
+++ lib/job_finder_app/job_finder_main_page.dart
@@ -23,9 +23,122 @@ class _JobFinderMainPageState extends State<JobFinderMainPage> {
         actions: [
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: (){},
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.tune),
+            onPressed: () {},
           )
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            Text('Designer'),
+            Text('Job'),
+            SizedBox(
+              height: 48,
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 34,
+                  width: 108,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Yangon'),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.clear),
+                        iconSize: 16,
+                      )
+                    ],
+                  ),
+                ),SizedBox(width: 16,),
+                Container(
+                  height: 34,
+                  width: 96,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Row(
+                    children: [
+                      Text('\$ 40/h'),
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {},
+                        icon: Icon(Icons.clear),
+                        iconSize: 16,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),SizedBox(height: 48,),
+            Container(
+              height: 240,
+              width: double.infinity,
+              color: Colors.blueGrey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('For you'),
+                  Container(
+                    height: 200,
+                    color: Colors.red,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width/2,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(8)
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 240,
+              width: double.infinity,
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Recently Added'),
+                  Container(
+                    height: 218,
+                    color: Colors.green,
+                    child: ListView.builder(itemBuilder: (context,index){
+                      return Container(
+                        height: 86,
+                        margin: EdgeInsets.only(bottom: 8),
+                        decoration: BoxDecoration(
+                          color: Colors.white
+                        ),
+                      );
+                    }),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
